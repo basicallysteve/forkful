236d3f264e432c3adfa0e311a4633c3dd71cef21
Steven Turner <steven.turner@rothco.com>
2025-12-02 22:08:46 -0500

    Implement user model and update root endpoint to return user data

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-
+from app.models.user import User
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
     # should return public/index.html content
-    return {"message": "Welcome to the Forkful api!"}
+    return User(user_id=1, username="testuser", email="test@gmail.com")
 
 
