6626ded9929223570a516578473a550a8f20221d
Steven Turner <steven.turner@rothco.com>
2025-12-10 09:43:15 -0500

    Refactor PantryItem type to use ServingSize for originalSize and currentSize

--- src/types/PantryItem.ts
+++ src/types/PantryItem.ts
@@ -2,14 +2,21 @@ import type { Food } from './Food'
 
 export type PantryItemStatus = 'expired' | 'expiring-soon' | 'good'
 
+type ServingSize = {
+  size: number
+  unit?: string
+  breaksInto?: ServingSize
+}
+
+
 export type PantryItem = {
   id: number
   food: Food
   expirationDate: Date | null
   quantity: number
   quantityLeft: number
-  originalSize: number
-  currentSize: number
+  originalSize: ServingSize
+  currentSize: ServingSize
   addedDate: Date
   status: PantryItemStatus
 }
