090fb9b7273d81384278039a8bf45eb32f814a67
Steven Turner <steven.turner@rothco.com>
2026-06-09 19:58:09 -0400

    fix: add console log for database URL in drizzle configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
 
 // config({ path: process.env.DRIZZLE_ENV === 'production' ? '.env.production' : '.env' });
-
+console.log('Using database URL:', process.env.DATABASE_POSTGRES_URL);
 export default defineConfig({
   schema: './src/db/schema.ts',
   out: './drizzle',
